269e305b9dca1f309ec396a02735de93f4c56e85
ibn5x <kiakilipictures@gmail.com>
2021-07-30 10:05:08 -0400

    compile, migration, and deployment to truffle dev blockchain success

--- migrations/2_deploy_proxy.js
+++ migrations/2_deploy_proxy.js
@@ -2,8 +2,10 @@ const Cat = artifacts.require("Cat");
 const Proxy = artifacts.require("Proxy");
 
 module.exports = async function (deployer, network, accounts) {
-  const Cats = await Cat.new;
-  const proxy = await Proxy.new(dogs.address);
+  
+    const swappable = await Cat.new();
+    const proxer = await Proxy.new(swappable.address);
+  
 
 //  deployer.deploy(Migrations);
 //  deployer.deploy(Migrations);
