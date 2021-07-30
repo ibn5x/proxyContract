cd64cfd11e770d09b784c6cbd962aa2530c88844
ibn5x <kiakilipictures@gmail.com>
2021-07-30 10:50:41 -0400

    reroute to functional contract testing

--- migrations/2_deploy_proxy.js
+++ migrations/2_deploy_proxy.js
@@ -3,10 +3,25 @@ const Proxy = artifacts.require("Proxy");
 
 module.exports = async function (deployer, network, accounts) {
   
-    const swappable = await Cat.new();
-    const proxer = await Proxy.new(swappable.address);
+    const cat = await Cat.new();
+    const proxy = await Proxy.new(cat.address);
   
+/*
+    Here we are fooling truffle to believe that the proxy contract
+    is really the functional contract. This statement takes the Cat source code 
+    
+    This statement takes the Cat source code and we use the truffle function .at to 
+    basically tell truffle "Hey I already have a Cat contract, its deployed at this address,
+    so create a 'new' instance of Cat contract but do it from my already existing deployed contract"
+    
+    then create a new variable with that address instance
 
-//  deployer.deploy(Migrations);
-//  deployer.deploy(Migrations);
+    */
+ 
+     var proxyCat = await Cat.at(proxy.address); 
+     await proxyCat.setTheNumberOfCats(7);
+
+    var numberOfCats = await proxyCat.getTheNumberOfCats();
+    console.log(numberOfCats.toNumber()); //convert bignumber
+
 };
